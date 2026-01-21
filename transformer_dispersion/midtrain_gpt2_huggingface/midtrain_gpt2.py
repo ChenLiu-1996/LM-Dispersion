e04dd56b04ce76baff34256ecc0c30b2a6caddaf
Chen Liu <chen.liu.cl2482@yale.edu>
2026-01-20 22:42:51 -0500

    minor

--- transformer_dispersion/midtrain_gpt2_huggingface/midtrain_gpt2.py
+++ transformer_dispersion/midtrain_gpt2_huggingface/midtrain_gpt2.py
@@ -555,7 +555,7 @@ if __name__ == "__main__":
     ap.add_argument("--no_save_model", action="store_true")
     ap.add_argument("--num_workers", type=int, default=8, help="Number of dataloader workers.")
     ap.add_argument("--per_device_train_batch_size", type=int, default=16)
-    ap.add_argument("--gradient_accumulation_steps", type=int, default=4)
+    ap.add_argument("--gradient_accumulation_steps", type=int, default=32)
     ap.add_argument("--seed", type=int, default=1)
 
     args = ap.parse_args()
